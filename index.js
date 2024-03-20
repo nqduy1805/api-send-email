bacc94eefd04b4eae61134d04e16aceef67ba44e
nqduy1805 <tnu1805@gmail.com>
2024-03-20 15:15:34 +0700

    u

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function display(name){
-    console.log(name)
-    return name;
-}
-display('api-send-email2git');
+var twoSum = function(nums, target) {
+    for(let i = 0 ; i < nums.length-1;i++){
+       for(let j = i+1 ; j < nums.length;j++){
+        if(nums[i]+nums[j] == target){
+            return [i,j];
+        }
+        } 
+    }
+};
+console.log(twoSum([2,7,11,15],9));
